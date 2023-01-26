ce5ce4e683832467722d585548f167606204c92a
dxMaria <mariya.nikulina@devexpress.com>
2023-01-26 22:43:48 +0400

    Update to NET 6

--- VB/My Project/AssemblyInfo.vb
+++ VB/My Project/AssemblyInfo.vb
@@ -8,14 +8,6 @@ Imports System.Windows
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("ExportOnlyBodyContent")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("Microsoft")>
-<Assembly: AssemblyProduct("ExportOnlyBodyContent")>
-<Assembly: AssemblyCopyright("Copyright © Microsoft 2010")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
 
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
@@ -49,5 +41,3 @@ Imports System.Windows
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
